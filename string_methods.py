5c041a5f7020452099b00bdf39d6c7452dc8dbd7
opilane <opilane@PC215-10.(none)>
2013-09-12 08:39:07 +0300

    Bla

---
+++ string_methods.py
@@ -0,0 +1,63 @@
+
+print "abc".capitalize()
+print "abcd".center(20,"-")
+print "abcde".count("a", 0, 100)
+print "abc".encode('UTF-8','strict')
+print "abc".decode('UTF-8','strict')
+str = "minu esimene progemine"
+print str.endswith("progemine")
+print str.endswith("progemine",20)
+print str.endswith("esimene",2,10)
+abc = "esimene progemine"
+print abc.expandtabs(0)
+print abc.expandtabs(4)
+print str.find("esi",1)
+print str.find("prog",1)
+abc = "{} saia {}"
+print abc.format("sooja","sisu")
+print str.index("si",1)
+print str.index("og",1)
+print "12defj4".isalnum()
+print "12defj4!".isalnum()
+print "12defj4".isalpha()
+print "12defj4!".isalpha()
+print "12defj4".isdigit()
+print "1233216".isdigit()
+print "12defj4".islower()
+print "12defj4!".islower()
+print "12defj4".isspace()
+print "12defj4!".isspace()
+print "12defj4".istitle()
+print "12defj4!".istitle()
+print "12defj4".isupper()
+print "12defj4!".isupper()
+#join
+print str.ljust(25, "@")
+aja = "AdaAJIUjhJ"
+print aja.lower()
+jama = "oomknjsaaaakn kskmkmiiaaa000jjj"
+print jama.lstrip("a")
+print jama.partition("0")
+print str.replace("a", "b",3)
+print str.rfind("ja",0,10)
+print str.rindex("esimene")
+print str.rjust(25,"%")
+print jama.rpartition("0")
+print jama.rstrip("a")
+print "1 2 3 4".rsplit(None,1)
+print "1 2 3 4".split()
+print "ab c\nde fg\rkl\r\n".splitlines()
+print "asfasfas".startswith("a",0,20)
+print "aasdasd".strip("a")
+print "asdffsJHGasdj".swapcase()
+print "asd asd farw asd".title()
+#translate
+print "asdHGhg".upper()
+print "kjashd asdff sdfsdfsdf sd asd".zfill(50)
+#isnumeric
+#isdecimal
+
+
+
+
+
