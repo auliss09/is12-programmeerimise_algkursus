0ca07455331ed22b6dbb5f2448f0ec916eaa33a0
opilane <opilane@PC215-10.(none)>
2013-09-12 09:53:31 +0300

    kjkl

---
+++ strings.py
@@ -0,0 +1,26 @@
+print "spam eggs"
+print "doesn\'t"
+print "doesn't"
+print '"Jah," ta ytles.'
+print "\"Yes,\" he said."
+print '"Isn\'t," she said.'
+
+hello = "This is a rather long string containing\nseveral lines of text just as you would do in C.\n    Note that whitespace at the beginning of the line is\
+ significant."
+print hello
+
+print """
+K2sk: git [options]
+	-push		2leslaadimiseks
+	-pull		allalaadimiseks
+"""
+
+hello = r"See string on ysna pikk, kuid\n ei vii teksti uuele reale, sest jutum2rkide ees on t2ht r"
+print hello
+
+word = 'Abi' + 'A'
+print word
+print '<' + word*5 + '>'
+
+print 'str' 'ing'
+print 'str'.strip() + 'ing'
