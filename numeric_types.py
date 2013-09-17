5f3664c23925e148047731eeead4a42e16f6f1ae
Aulis <aulis.s09@gmail.com>
2013-09-17 13:15:35 +0300

    Bla

--- numeric_types.py
+++ numeric_types.py
@@ -6,25 +6,25 @@ print 2//8
 print 3%5
 print -6
 print +6
-print abs (5)
-print int (3)
-print long (3)
+print abs(5)
+print int (3.4)
+print long (3) 
 print float (9)
-print complex (5,6)
-print divmod (5, 6)
+print divmod (5, 6) 
 print pow (7, 9)
-print 3 ** 3
+print 3 ** 3 
 
-print 4 | 8
-print 37^16
+print  4 | 8
+print 37^16 
 print 7&15
-print 14<<7
+print 14<<7 
 print 14>>7
 print ~ 9
 
 n= -26
 print bin (n)
-print n.bit_length ()
+n.bit_length ()
+
 
 print (-6.0) .is_integer()
 print (5.4) .is_integer()
@@ -32,4 +32,5 @@ print (5.4) .is_integer()
 print float.fromhex ('0x3.a7p10')
 
 print float.hex (3740.0)
+
 
