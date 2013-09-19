3c5880cf7ffd44a627a86ae9d4cfca7a897a1edd
opilane <opilane@PC215-10.(none)>
2013-09-19 09:01:32 +0300

    valemid

---
+++ valemid.py
@@ -0,0 +1,58 @@
+# v=s/t
+S = 56.0
+T = 85.0
+V = S/T
+print V
+
+# p=2*(a+b)
+B = 13.0
+P = 2*(A+B)
+print P
+
+# Silindri ruumala
+pii=3.14
+R=69
+H=12
+V=(1/3)*pii*(R*R)*H
+
+# pythagoras
+a=13
+a1=13*a
+b=21 
+b1=21*b
+cruudus= (a1+b1)
+print c 
+
+# e=mc^2
+M = 13.0
+C = 5.0
+E = M*C*C
+print E
+
+# püstprisma St
+Sk=76
+Sp=53
+St=Sk+(2*Sp)
+print St
+
+# i=u/r
+U = 317.0
+R = 43.0
+I = U/R
+print I
+
+# v=a^3
+A = 7.0
+V1 = A*A*A
+print V1
+
+# koonuse St
+pii=3.14
+R=25
+m=5
+kSt=pii*R*(R+m)
+
+# sk=n*a*m/2
+N = 14.0
+SK = (N*A*M)/2
+print SK
