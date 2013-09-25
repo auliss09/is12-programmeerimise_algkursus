9071e5a9b9e2e9a0308a7a06bf06e0f27d1e909a
aulis <aulis.s09@gmail.com>
2013-09-25 15:37:07 +0300

    tood

--- tunnitoo5-1.py
+++ tunnitoo5-1.py
@@ -3,15 +3,16 @@
 from random import randint
 
 kuulid = ['s']*5 + ['m']*6 + ['v']*7
-
-n = len(kuulid)-1
-kuul = randint(0,n)
+min = 0
+max = len(kuulid)-1
+kuul = randint(min, max)
 kuul1 = kuulid[kuul]
 del kuulid[kuul]
 print kuul1
 
-n = len(kuulid)-1
-kuul = randint(0,n)
+min = 0
+max = len(kuulid)-1
+kuul = randint(min, max)
 kuul2 = kuulid[kuul]
 del kuulid[kuul]
 print kuul2
