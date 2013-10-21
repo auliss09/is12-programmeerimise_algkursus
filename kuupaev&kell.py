4bf0348d42bbbbc11e2b32ef493026322f508a6d
aulis <aulis.s09@gmail.com>
2013-10-21 15:33:17 +0300

    kuupaev&kellaaeg

--- kuupaev&kell.py
+++ kuupaev&kell.py
@@ -6,4 +6,11 @@ current_year = now.year
 current_hour = now.hour
 current_minute = now.minute
 current_second = now.second
-print str(current_month) + "/" + str(current_day) + "/" + str(current_year) + " " + str(current_hour) + ":" + str(current_minute) + ":" + str(current_second)
+print str(current_day) + "/" + str(current_month) + "/" + str(current_year) + " " + str(current_hour) + ":" + str(current_minute) + ":" + str(current_second)
+
+print ""
+
+import time
+Time = str(time.strftime ('%H:%M:%S'))
+Date = str(time.strftime ('%a %d.%m.%Y'))
+print Date + " " + Time
