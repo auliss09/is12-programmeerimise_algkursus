dc21070b1e89e4f20f4e3040511eab6606de2007
aulis <aulis.s09@gmail.com>
2013-12-04 14:09:21 +0200

    kodut66

---
+++ kodut66_4_12_2013.py
@@ -0,0 +1,12 @@
+i = 0
+while i < 5:
+    j = 0
+    while j <= i:
+        print '$',
+        j = j + 1
+    j = 5
+    while i < j:
+       	print "#",
+        j = j - 1
+    print
+    i = i + 1
