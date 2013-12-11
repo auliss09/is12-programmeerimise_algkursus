0114d271becd495ea43102f1af73679bad3f16a5
aulis <aulis.s09@gmail.com>
2013-12-11 14:07:29 +0200

    kast - kodut66

---
+++ kasti_printimine.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+def ruut(h, w):
+    j=0
+    while j < h:
+        i=0
+        j=j+1
+        while i < w:
+            print "#",
+            i=i+1
+        print
+ruut(10, 10)
